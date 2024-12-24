d6436be3ae47dbe97aa3c48147194c5e089d194f
Matteo Gagliardi <gagliardimatteo8@gmail.com>
2024-12-24 12:01:34 +0100

    frontend responsive

--- brainy-front/src/app/menu/menu.component.ts
+++ brainy-front/src/app/menu/menu.component.ts
@@ -9,6 +9,7 @@ import { Component, HostListener } from '@angular/core';
 export class MenuComponent {
 
   isScrolled = false; // Variabile per tracciare lo stato dello scroll
+  menuOpened = false;
 
   // Listener per lo scroll della finestra
   @HostListener('window:scroll', [])
@@ -16,6 +17,12 @@ export class MenuComponent {
     // Aggiunge una classe se l'utente ha scrollato verso il basso
     const scrollOffset = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
     this.isScrolled = scrollOffset > 0;
+  }
+
+  openMenu(): void{
+    if(!this.isScrolled){
+      this.menuOpened = !this.menuOpened
+    }    
   }
 
 }
